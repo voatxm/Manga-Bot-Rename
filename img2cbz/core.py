fc44dbd86f22f85c8ca36a423694b63c4d275618
VØAT <71224541+voatxm@users.noreply.github.com>
2024-10-17 10:17:10 +0530

    Update core.py

--- img2cbz/core.py
+++ img2cbz/core.py
@@ -8,8 +8,6 @@ def fld2cbz(folder: Path, name: str):
     cbz = folder / f'{name}.cbz'
     files = [file for file in folder.glob(r'*') if re.match(r'.*\.(jpg|png|jpeg|webp)', file.name)]
     files.sort(key=lambda x: x.name)
-    files.insert(0, "first.jpg")
-    files.append("last.jpg")
     img2cbz(files, cbz)
     return cbz
 
