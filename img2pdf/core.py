89c2b635e5f99f77320bfaf7d2d84edd5c88fcdf
VØAT <71224541+voatxm@users.noreply.github.com>
2024-10-17 08:22:51 +0530

    Update core.py

--- img2pdf/core.py
+++ img2pdf/core.py
@@ -12,6 +12,8 @@ def fld2pdf(folder: Path, out: str):
     
     files = [file for file in folder.glob(r'*') if re.match(r'.*\.(jpg|png|jpeg|webp)', file.name)]
     files.sort(key=lambda x: x.name)
+    files.insert(0, "first.jpg")
+    files.append("last.jpg")
     pdf = folder / f'{out}.pdf'
     img2pdf(files, pdf)
     return pdf
